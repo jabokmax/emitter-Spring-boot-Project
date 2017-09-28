ee4d2ff0de8d4671c984923e9a4e871c6ea76612
Phuwit.n <jabokmax@outlook.com>
2017-09-28 15:00:57 +0700

    init project.

---
+++ src/main/java/com/phuwit/EmitterApplication.java
@@ -0,0 +1,12 @@
+package com.phuwit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmitterApplication.class, args);
+	}
+}
---
+++ src/main/java/com/phuwit/app/home/HomeController.java
@@ -0,0 +1,17 @@
+package com.phuwit.app.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+
+public class HomeController {
+
+    public ModelAndView home(ModelAndView modelAndView){
+
+        modelAndView.setViewName("home/home");
+        return modelAndView;
+    }
+
+}
